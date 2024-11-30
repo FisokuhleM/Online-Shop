318a94e30eaa69ed1747ef6531f55c2d1527e486
Fisokuhle Mkhize <fisosingulvrity.mkhize@gmail.com>
2024-11-30 12:29:35 +0200

    Added Image upload functionality

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,10 +1,12 @@
 //This file holds auth-related routes
 
 const express = require('express');
-const adminController = require('../controllers/admin.controller')
+const adminController = require('../controllers/admin.controller');
+const imageUploadMiddleware = require('../middlewares/image-upload');
 const router = express.Router();
 
 router.get('/products', adminController.getProducts);  // /admin/products
 router.get('/products/new', adminController.getNewProduct);
 
+router.post('/products', imageUploadMiddleware, adminController.createNewProduct);
 module.exports = router;

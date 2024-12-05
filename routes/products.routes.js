1842d04e235f63bda31ef770866c0082cd9c8f3c
Fisokuhle Mkhize <fisosingulvrity.mkhize@gmail.com>
2024-12-05 23:22:09 +0200

    Implemented Customer Views for Product and Product Details

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -1,13 +1,13 @@
 //This file holds auth-related routes
 
 const express = require('express');
+const productsController = require('../controllers/products.controller');
 
 //Get the controller
 
 const router = express.Router();
 
-router.get('/products', function(req, res) {
-  res.render('customer/product/all-products');
-});
+router.get('/products', productsController.getAllProducts);
+router.get('/products/:id', productsController.getProductDetails);
 
 module.exports = router;

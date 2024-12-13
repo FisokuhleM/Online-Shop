ac8574c1221ab76c6b89e320209d7ed5ed875e1b
Fisokuhle Mkhize <fisosingulvrity.mkhize@gmail.com>
2024-12-13 23:28:58 +0200

    Implemented Order display(For Admin and Customers)

--- models/order.model.js
+++ models/order.model.js
@@ -1,38 +1,93 @@
+const mongodb = require('mongodb');
+
 const db = require('../data/database');
 
-class Order{
-    //Status => pending, fulfilled, cancelled
-    constructor(cart, userData, status='pending', date, orderId){ //2025-07-16
-        this.productData = cart;
-        this.userData = userData;
-        this.status = status;
-        this.date = new Date(date);
-        if(this.Date){
-            this.formattedDate = this.date.toLocaleDateString('en-US',{
-                weekday:'short',
-                day:'numeric',
-                month: 'long',
-                year:'numeric',
-            });
-        }
-        this.id = orderId;
+class Order {
+  // Status => pending, fulfilled, cancelled
+  constructor(cart, userData, status = 'pending', date, orderId) {
+    this.productData = cart;
+    this.userData = userData;
+    this.status = status;
+    this.date = new Date(date);
+    if (this.date) {
+      this.formattedDate = this.date.toLocaleDateString('en-US', {
+        weekday: 'short',
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric',
+      });
     }
+    this.id = orderId;
+  }
 
-    //Saving the order to the database 
-    save(){
-        if(this.id){
-            //Updating
-        }else{
-            const orderDocument = {
-               userData: this.userData,
-              productData: this.productData,
-              date: new Date(),
-              status: this.status,
-            };
+  //Where attributes of the orderDoc will be mapped to each attribute of the Order class.
+  static transformOrderDocument(orderDoc) {
+    return new Order(
+      orderDoc.productData,
+      orderDoc.userData,
+      orderDoc.status,
+      orderDoc.date,
+      orderDoc._id
+    );
+  }
 
-            return db.getDb().collection('orders').insertOne(orderDocument);
-        }
+  //Helper Method--> which should map all order documents to an Object of the Order class
+  static transformOrderDocuments(orderDocs) {
+    return orderDocs.map(this.transformOrderDocument);
+  }
+
+  //Find all logged orders
+  static async findAll() { 
+    const orders = await db
+      .getDb()
+      .collection('orders')
+      .find()
+      .sort({ _id: -1 })
+      .toArray();
+
+    return this.transformOrderDocuments(orders);
+  }
+
+  static async findAllForUser(userId) {
+    const uid = new mongodb.ObjectId(userId);
+
+    const orders = await db
+      .getDb()
+      .collection('orders')
+      .find({ 'userData._id': uid }) 
+      .sort({ _id: -1 }) //Descending order 
+      .toArray();
+
+    return this.transformOrderDocuments(orders);
+  }
+
+  static async findById(orderId) {
+    const order = await db
+      .getDb()
+      .collection('orders')
+      .findOne({ _id: new mongodb.ObjectId(orderId) });
+
+    return this.transformOrderDocument(order);
+  }
+
+  save() {
+    if (this.id) {
+      const orderId = new mongodb.ObjectId(this.id);
+      return db
+        .getDb()
+        .collection('orders')
+        .updateOne({ _id: orderId }, { $set: { status: this.status } });
+    } else {
+      const orderDocument = {
+        userData: this.userData,
+        productData: this.productData,
+        date: new Date(),
+        status: this.status,
+      };
+
+      return db.getDb().collection('orders').insertOne(orderDocument);
     }
+  }
 }
 
-module.exports = Order
+module.exports = Order;
--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -15,5 +15,8 @@ router.post('/products/:id',imageUploadMiddleware, adminController.updateProduct
 
 router.delete('/products/:id',adminController.deleteProduct);
 
+router.get('/orders', adminController.getOrders);
+
+router.patch('/orders/:id', adminController.updateOrder);
 
 module.exports = router;
